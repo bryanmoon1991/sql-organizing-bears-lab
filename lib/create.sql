933d761ebe4b4aa76e01f53f624ae03f36e77e89
bryanmoon1991 <bryanmoon1991@gmail.com>
2020-09-22 18:45:07 -0400

    done

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age ITEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN 
+);
---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, gender)
+VALUES ('Mr.Chocolate', 'M');
+INSERT INTO bears (name, gender)
+VALUES ('Rowdy', 'M');
+INSERT INTO bears (name, gender)
+VALUES ('Tabitha', 'F');
+INSERT INTO bears (name, gender)
+VALUES ('Seargent Brown', 'F');
+INSERT INTO bears (name, gender)
+VALUES ('Melissa', 'F');
+INSERT INTO bears (name, gender)
+VALUES ('Grinch', 'F');
+INSERT INTO bears (name, gender)
+VALUES ('Wendy', 'F');
+INSERT INTO bears (gender)
+VALUES ('M');
